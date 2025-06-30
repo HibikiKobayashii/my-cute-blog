8631ca62e82b1f4e7e46855c3b1e7c9536ac6d8b
小林響 <kobayashikyou@Pixel-8-Pro.local>
2025-06-30 16:07:55 +0900

    Fix Vercel build error and dark mode styles

--- src/app/components/ThemeProvider.tsx
+++ src/app/components/ThemeProvider.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// ▼▼▼ このファイルのインポートと型指定を修正しました ▼▼▼
+export function ThemeProvider({ children, ...props }: React.PropsWithChildren<Omit<ThemeProviderProps, 'children'>>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
